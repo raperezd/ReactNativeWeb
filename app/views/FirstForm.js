69a00292d3acd2264ed5fb9563a13120275fc4cf
Diego Monroy <ingdiegomonroy@gmail.com>
2018-01-31 17:48:45 -0500

    Add file loader

--- app/views/FirstForm.js
+++ app/views/FirstForm.js
@@ -1,48 +1,9 @@
 import React, { Component } from 'react';
-/* import { Button, Card, CardItem, Body, List, ListItem } from 'native-base';
-
-import Icon from 'react-native-vector-icons/Feather';
-import BizagiGroup from './components/BizagiGroup';
-import BizagiControl from './components/BizagiControl';
-import BizagiExtendedText from './components/BizagiExtendedText';
-import BizagiGrid from './components/BizagiGrid';
-import ImageHeader from './components/ImageHeader';
-import TouchableIconBtn from './components/TouchableIconBtn';
-import BizagiCollapsible from './components/BizagiCollapsible'; */
-
-
-// const styles = StyleSheet.create({
-//     form: {
-//         backgroundColor: '#FFFFFF'
-//     },
-//     header: {
-//         backgroundColor: 'transparent'
-//         //height: 81
-//     },
-//     headerTitle: {
-//         color: '#FFF',
-//         fontSize: 16,
-//         fontWeight: '200'
-//     },
-//     headerSubTitle: {
-//         color: '#FFF',
-//         fontSize: 12,
-//         fontWeight: '100'
-//     },
-//     headerBackTitle: {
-//         fontSize: 16,
-//         padding: 16,
-//         color: '#FFF'
-//     }
-// });
-
 
 export default class FirstForm extends React.Component {
     render() {
-        const match = this.props.match;
         return (
-            <h2>First form</h2>
+            <h1>FirstForm</h1>
         );
     }
 }
- 
