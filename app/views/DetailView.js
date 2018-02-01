63cddc0e166e5bdb36c6407ae049fae2573f06d6
Diego Monroy <ingdiegomonroy@gmail.com>
2018-02-01 14:11:16 -0500

    DetalvIEW V1.1

--- app/views/DetailView.js
+++ app/views/DetailView.js
@@ -7,7 +7,7 @@ import Grid from '../components/grid/Grid';
 import Control from '../components/control/Control';
 import ExtendedText from '../components/extendedText/ExtendedText';
 
-import BizagiCounter  from '../components/counter/BizagiCounter'
+import BizagiCounter from '../components/counter/BizagiCounter'
 
 class DetailView extends React.Component {
 
@@ -19,9 +19,24 @@ class DetailView extends React.Component {
         const match = this.props.match;
         return (
             <Form>
+                <ul className="flex-container">
+                    <li className="detailImg">
+                        <img width="50"src="https://assets.bose.com/content/dam/Bose_DAM/Web/consumer_electronics/global/products/headphones/quietcomfort_25_acoustic_noise_cancelling_headphones-apple_devices/product_silo_images/qc25_apple_black_EC_hero_010716.psd/_jcr_content/renditions/cq5dam.web.320.320.png" alt=""/>
+                    </li>        
+                    <li className="flex-item">
+                        <h4>Headphones</h4>
+                         <span><b>Dimensions:</b>6.4 x 6.7 x 2.1 in</span>
+                         <span><b>Deliver to:</b>Julio Ocaña</span>
+                    </li>
+                    <li className="flex-item">
+                        <span className="time">Deliver in 3 days</span>
+                    </li>
+                </ul>
+                <hr size="1" />
                 <div className="quantitySection">
-                    <BizagiCounter/>
+                    <BizagiCounter />
                 </div>
+                <hr size="1" />
                 <div className="tab">
                     <button className="tablinks" onClick={this.openTab(event, 'Overview')}>Overview</button>
                     <button className="tablinks" onClick={this.openTab(event, 'Specs')}>Specs</button>
